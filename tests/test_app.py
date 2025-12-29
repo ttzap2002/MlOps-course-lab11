33736e018ada36882eaebcd648a08028045d0ff7
Tomasz Zapart <tomasz.t.zapart@gmail.com>
2025-12-29 19:23:31 +0100

    add tests

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+from fastapi.testclient import TestClient
+from sentiment_app.app import app
+import pytest
+
+client = TestClient(app)
+
+def test_welcome_root():
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json() == {"message": "Welcome to the ML API"}
